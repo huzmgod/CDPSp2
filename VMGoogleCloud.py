966dd5fafef06c69c3a910600cec0e817fe45677
huzmgod <ivan.perezd@alumnos.upm.es>
2022-01-23 03:41:52 +0100

    script done

--- VMGoogleCloud.py
+++ VMGoogleCloud.py
@@ -1,9 +1,9 @@
 import os, sys
 
 YES = "--assume-yes"
-DOWNLD_LINK = "something"
-FOLDER_PATH = "something"
-
+DOWNLD_LINK = "https://github.com/huzmgod/CDPSp2.git"
+FOLDER_PATH = "CDPSp2/bookinfo/src/productpage/"
+CUSTOM_PORT = 4000
 
 def main():
     os.system("sudo apt-get update")
@@ -12,3 +12,5 @@ def main():
     os.system(f"cd {FOLDER_PATH}")
     os.system(f"sudo apt-get {YES} install python3-pip")
     os.system("pip3 install -r requirements.txt")
+    os.system("export GROUP_NUMBER=46")
+    os.system(f"python3 productpage_monolithic {CUSTOM_PORT}")
